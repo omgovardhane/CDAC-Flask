205b76265be9e519d096dbe9478b152422fc61c0
omkar <Govardhaneomkar94@gmail.com>
2022-04-04 17:52:14 +0530

    Create down.py

---
+++ down.py
@@ -0,0 +1,10 @@
+from io import BytesIO
+from urllib.request import urlopen
+from zipfile import ZipFile
+zipurl = 'https://files.grouplens.org/datasets/movielens/ml-1m.zip'
+with urlopen(zipurl) as zipresp:
+    print('Downloading zip....')
+    with ZipFile(BytesIO(zipresp.read())) as zfile:
+        print('Extracting...')
+        zfile.extractall(r'D:\Dataset')
+print('Extracted...')
